784cddbc91f65c1654e86210b51d7946430829f1
@nickswanigan <nickswanigan04@gmail.com>
2025-08-07 16:27:10 -0500

    node.js functions added

--- server/server.js
+++ server/server.js
@@ -5,9 +5,17 @@ const app = express();
 // Serve React build
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+// Example API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+// React catch-all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
